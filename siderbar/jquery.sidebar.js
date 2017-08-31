0307e55feb5de7d10f682795c8ab84e8067e7166
lucy <luuck@users.noreply.github.com>
2017-08-31 20:14:15 +0800

    Update jquery.sidebar.js

--- siderbar/jquery.sidebar.js
+++ siderbar/jquery.sidebar.js
@@ -1,94 +1,134 @@
 /**
+ * v1.3.0
  * [右侧悬浮窗组插件]
  * 组件依赖：jquery1.7.2+
  */
+(function(){
 
-(function($) {
-    var scrollTop;
     var $sidebar;
-    var fromBottom = 0;
-    var isFixedTop;
-    var fixedTopDis;
     var isIe6 = 'undefined' == typeof(document.body.style.maxHeight);
-    var lr = {
-        scroll: function() {
-            var _scrollTop = $(window).height() / 2 - args.height / 2;
-            _scrollTop += $(window).scrollTop();
-            // scrollTop = isFixedTop ? $(window).scrollTop() + fixedTopDis : $(window).scrollTop() + fromBottom;
-            $sidebar.css('top', scrollTop + 'px');
+    var h;
+
+    function Sidebar(opts){
+        this._opts = opts || {};
+        this._opts.wrapper = opts.wrapper;
+        this._opts.pageW = opts.pageW;
+        this._opts.width = opts.width || 30;
+        this._opts.height = opts.height || 100;
+        this._opts.pageContentWidth = opts.pageContentWidth || 0;
+        this._opts.lrDistance = opts.lrDistance || 0;
+        this._opts.tbDistance = opts.tbDistance || 0;
+        this._opts.position = opts.position || ['right','middle'];
+        this._opts.scrollTopShow = opts.scrollTopShow || 0;
+        this._opts.scrollfn = function(){};
+        this._opts.callfn = opts.callfn || function(){};
+        this._init(this._opts);
+    }
+
+    Sidebar.prototype = {
+        _scroll: function(args){
+            var _a;
+            $sidebar = $(args.wrapper);
+
+            if(args.position[1].toLowerCase() === 'bottom') {
+                _a = $(window).height() + $(window).scrollTop() - $sidebar.height() - args.tbDistance;
+                $sidebar.css({'top': _a});
+                return;
+
+            }else if(args.position[1].toLowerCase() === 'top'){
+                _a = $(window).scrollTop() + args.tbDistance ;
+                if(_a < args.scrollTopShow){
+                    $sidebar.css({'top': args.scrollTopShow});
+                }else{
+                    $sidebar.css({'top': _a});
+                }
+                return;
+            }
+
+            var _scrollTop = $(window).height() / 2 - h / 2;
+            _scrollTop = _scrollTop + $(window).scrollTop();
+            $sidebar.css({ 'top': _scrollTop } );
         },
-        initCss: function(args) {
-            var w = $(window).width(); //浏览器当前窗口可视区域宽度
-            if (args.pageContentWidth > 0 && w > 980) {
-                args.marginRight = (w - args.pageContentWidth) / 2 - args.width;
+        _initCss: function(args){
+            var ww = $(window).width(); //浏览器当前窗口可视区域宽度
+            var w = $(args.pageW).width();
+            $sidebar = $(args.wrapper);
+            if (args.pageContentWidth > 0 ) {
+                args.lrDistance = (ww - args.pageContentWidth)/2;
+                args.lrDistance = isIe6 ? (w - args.pageContentWidth) / 2 : args.lrDistance;
             }
 
             $sidebar.css({
                 'width': args.width + 'px',
-                'height': +args.height + 'px',
-                'right': args.marginRight + 'px',
-                'background-color': args.color
+                'height': +args.height + 'px'
             });
+
+            if(args.position[0].toLowerCase() === 'left'){ 
+                $sidebar.css({
+                    'left': args.lrDistance + 'px'
+                });
+            }else{
+                $sidebar.css({
+                    'right': args.lrDistance + 'px'
+                });
+            }
         },
-        isScrollLoad: function(args) {
-            if (args.isScrollShow) {
-                $(window).off('scroll').scroll(function() {
-                    lr.initCss(args);
-                    var _scrollTop = $(window).scrollTop();
-                    if (_scrollTop < 100) {
-                        $sidebar.hide();
-                    } else {
-                        $sidebar.show();
-                    }
+        _isShow: function(args){
+            var _this = this;
+            _this._initCss(args);
+            $sidebar = $(args.wrapper);
+            args.callfn();
+            /*var _scrollTop = $(window).scrollTop();
+            var _num = args.scrollTopShow;
+
+            if (_scrollTop < _num) {
+                $sidebar.hide();
+            } else {
+                $sidebar.show();
+            }*/
+
+        },
+        _isScrollLoad: function(args){
+            var _this = this;
+            if (args.scrollTopShow) {
+                _this._isShow(args);
+                $(window).scroll(function() {
+                    _this._isShow(args);
                 });
 
             } else {
-                lr.initCss(args);
+                _this._initCss(args);
+            }
+        },
+        _init: function(args){
+            var _this = this;
+            $sidebar = $(args.wrapper);
+            h = args.height;
+
+            _this._isScrollLoad(args);
+
+            // ie6特殊处理
+            if(isIe6){
+                $sidebar.css('position','absolute');
+                _this._scroll(args);
+                $(window).scroll(function() {
+                    _this._scroll(args);
+                });
+                return;
             }
 
-        },
-        init: function(obj, args) {
-            $sidebar = obj;
-            fromBottom = args.fromBottom;
-            isFixedTop = args.isFixedTop;
-            fixedTopDis = args.fixedTopDis;
+            if(args.position[1].toLowerCase() === 'top'){ // 固定在顶部
+                $sidebar.css({ 'top': args.tbDistance, 'position': 'fixed' });
 
-            lr.isScrollLoad(args);
+            }else if(args.position[1].toLowerCase() === 'bottom'){ // 固定在底部
+                $sidebar.css({ 'bottom': args.tbDistance, 'position': 'fixed' });
 
-            if (isIe6) { // ie6特殊处理
-                $sidebar.css('position', 'absolute');
-                lr.scroll();
-                window.onscroll = lr.scroll;
-            } else {
-                if (args.isFixedTop) {
-                    $sidebar.css({ 'top': args.fixedTopDis, 'position': 'fixed' });
-                    return;
-                }
-                // var _scrollTop = $(window).height() / 2 - fromBottom;
+            }else{ // 居中显示
                 var _scrollTop = $(window).height() / 2 - args.height / 2;
                 $sidebar.css({ 'top': _scrollTop, 'position': 'fixed' });
             }
-
-            args.backFn(); // 回调函数
         }
     };
 
-    $.fn.sidebar = function(options) {
-        return this.each(function() {
-            var args = $.extend({}, {
-                width: 200,
-                height: 300,
-                pageContentWidth: 980,
-                marginRight: 0, //  悬浮窗和页面右侧间隔距离
-                isFixedTop: false, // 是否固定在顶部
-                fixedTopDis: 0,
-                isScrollShow: false, // 距离顶部低于100隐藏
-                fromBottom: 130, // 悬浮窗和页面底部的间隔距离
-                color: '',
-                backFn: function() {}
-            }, options);
-            $(this).css('z-index',100);
-            lr.init($(this), args);
-        });
-    };
-})(jQuery);
+    window.Sidebar = Sidebar;
+})();
